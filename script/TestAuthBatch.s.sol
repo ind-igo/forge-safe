e01e995a644be8146d63b4804550e85a1258a8de
Oighty <oighty6@gmail.com>
2023-11-03 19:37:25 -0500

    refactor: test batches to use new interface

--- script/TestAuthBatch.s.sol
+++ script/TestAuthBatch.s.sol
@@ -10,11 +10,10 @@ interface IRolesAdmin {
 /// @notice A test for Gnosis Safe batching script
 /// @dev    GOERLI
 contract TestAuthBatch is BatchScript {
-    address safe = 0x84C0C005cF574D0e5C602EA7b366aE9c707381E0;
     address deployer = 0x1A5309F208f161a393E8b5A253de8Ab894A67188;
 
     /// @notice The main script entrypoint
-    function run(bool send_) external {
+    function run(bool send_) external isBatch(0x84C0C005cF574D0e5C602EA7b366aE9c707381E0) {
         IRolesAdmin rolesAdmin = IRolesAdmin(0x54FfCA586cD1B01E96a5682DF93a55d7Ef91EFF0);
 
         // Start batch
@@ -33,6 +32,6 @@ contract TestAuthBatch is BatchScript {
         ));
 
         // Execute batch
-        executeBatch(safe, send_);
+        executeBatch(send_);
     }
 }
